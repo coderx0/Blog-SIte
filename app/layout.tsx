7acefcaccd09fda6c264bff90cba1fc6df4d15fc
coderx0 <sumeetdeb100@gmail.com>
2024-08-04 20:06:58 +0530

    added daisyi ui and started building homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,64 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="night">
+      <body className={inter.className}>
+        <div className="drawer drawer-end">
+          <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col">
+            {/* Navbar */}
+            <div className="navbar bg-base-300 w-full">
+              <div className="mx-2 flex-1 px-2">Navbar Title</div>
+              <div className="hidden flex-none lg:block">
+                <ul className="menu menu-horizontal">
+                  {/* Navbar menu content here */}
+                  <li>
+                    <a>Navbar Item 1</a>
+                  </li>
+                  <li>
+                    <a>Navbar Item 2</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex-none lg:hidden">
+                <label
+                  htmlFor="my-drawer-3"
+                  aria-label="open sidebar"
+                  className="btn btn-square btn-ghost">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block h-6 w-6 stroke-current">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"></path>
+                  </svg>
+                </label>
+              </div>
+            </div>
+            {/* Page content here */}
+            <main className="px-6 bg-neutral">{children}</main>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="close sidebar"
+              className="drawer-overlay"></label>
+            <ul className="menu bg-base-200 min-h-full w-80 p-4">
+              {/* Sidebar content here */}
+              <li>
+                <a>Sidebar Item 1</a>
+              </li>
+              <li>
+                <a>Sidebar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,73 @@
 import { client } from "./utils/sanity/client";
-import {
-  PortableText,
-  PortableTextReactComponents,
-  PortableTextMarkComponentProps,
-} from "@portabletext/react";
 import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder(client);
-
 function urlFor(source: any) {
   return builder.image(source);
 }
 
-const ptComponents = {
-  types: {
-    image: ({ value }: { value: any }) => {
-      if (!value?.asset?._ref) {
-        return null;
-      }
-      return (
-        <div className="w-full flex justify-center items-center my-6">
-          <img
-            alt={value.alt || " "}
-            loading="lazy"
-            src={urlFor(value)
-              .width(320)
-              .height(240)
-              .fit("max")
-              .auto("format")
-              .url()}
-            className="w-full h-[340px] object-cover"
-          />
-        </div>
-      );
-    },
-  },
-  marks: {
-    link: ({ children, value }: PortableTextMarkComponentProps<any>) => {
-      return (
-        <a
-          href={value?.href}
-          rel={"noindex nofollow"}
-          target="_blank"
-          className="text-blue-700">
-          {children}
-        </a>
-      );
-    },
-  },
-};
-
-// Fetch content with GROQ
 const CONTENT_QUERY = `*[_type == "post"] {
-  ...,
-  author->,
-  categories[]->,
-  tags[],
-  content[] {
-    _type,
-    ...,
-    defined(string) => string
-  }
+  title,
+  slug,
+  thumbnail,
+  description
 }
 `;
 
 export default async function Home() {
-  const result = await client.fetch(CONTENT_QUERY);
-  console.log(result.length);
+  const [imp, ...result] = await client.fetch(CONTENT_QUERY);
+
   return (
-    <main className="w-full">
-      <div className="mx-12 md:mx-[300px] mt-12 text-justify flex flex-col gap-4 text-lg">
-        <PortableText value={result[2].content} components={ptComponents} />
+    <div className="">
+      <div className="h-[650px] flex gap-4 pt-4">
+        <div className="w-[20%]">
+          <h1 className="text-3xl font-bold">Trending</h1>
+          <div className="flex flex-col mt-6 gap-8 text-justify">
+            {result.map((blog: any) => (
+              <div key={blog.title}>
+                <img
+                  src={urlFor(blog.thumbnail).width(200).height(120).url()}
+                  alt="Thumbnail"
+                  className="w-full"
+                />
+                <p className="mt-4">{blog.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-[50%] px-8 ">
+          <div className="w-full mt-[60px]">
+            <img
+              src={urlFor(imp.thumbnail).width(400).height(280).url()}
+              alt="Thumbnail"
+              className="w-full"
+            />
+            <p className="mt-4 font-bold">{imp.title}</p>
+            <p className="mt-2">{imp.description}</p>
+          </div>
+        </div>
+        <div className="w-[30%]">
+          <h2 className="text-3xl font-bold text-right">Latest</h2>
+          <div className="flex flex-col mt-6 gap-8 text-justify">
+            {[imp, ...result].map((blog: any) => (
+              <div
+                key={blog.title}
+                className="flex gap-4 justify-center h-[120px]">
+                <div className="w-[200px]">
+                  <img
+                    src={urlFor(blog.thumbnail).width(140).height(140).url()}
+                    alt="Thumbnail"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="flex flex-1 justify-center items-center">
+                  <h2 className="">{blog.title}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
